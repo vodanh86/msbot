e7a2a79d2a5af04bf918540e6e00c0ae001609d9
cuongdm <cuongdm172@gmail.com>
2025-04-09 09:27:38 +0700

    init

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# filepath: /Users/cuongdm/Documents/repo/projects/LangchainDemo1/bot/Dockerfile
+FROM node:18 AS BUILD_IMAGE
+
+RUN curl -sfL https://gobinaries.com/tj/node-prune | bash -s -- -b /usr/local/bin
+
+WORKDIR /app
+
+COPY . /app/
+
+# install with legacy-peer-deps
+RUN rm -rf node_modules package-lock.json && npm install --legacy-peer-deps
+
+# build
+RUN npm run build
+
+# remove development dependencies
+RUN npm prune --production  --legacy-peer-deps
+
+# run node prune
+RUN /usr/local/bin/node-prune
+
+FROM node:18-alpine
+
+WORKDIR /app
+
+# copy from build image
+COPY --from=BUILD_IMAGE /app/dist ./dist
+COPY --from=BUILD_IMAGE /app/node_modules ./node_modules
+
+ENV DEBUG msteams
+
+EXPOSE 3007
+
+CMD [ "node", "dist/server.js" ]
---
+++ src/server/abHrBot/dialogs/mainDialog.ts
@@ -0,0 +1,131 @@
+import {
+    ComponentDialog,
+    DialogSet,
+    DialogState,
+    DialogTurnResult,
+    DialogTurnStatus,
+    TextPrompt,
+    WaterfallDialog,
+    WaterfallStepContext
+} from "botbuilder-dialogs";
+import {
+    MessageFactory,
+    StatePropertyAccessor,
+    InputHints,
+    TurnContext
+} from "botbuilder";
+import { TeamsInfoDialog } from "./teamsInfoDialog";
+import { HelpDialog } from "./helpDialog";
+import { MentionUserDialog } from "./mentionUserDialog";
+
+const MAIN_DIALOG_ID = "mainDialog";
+const MAIN_WATERFALL_DIALOG_ID = "mainWaterfallDialog";
+
+export class MainDialog extends ComponentDialog {
+    public onboarding: boolean;
+    constructor() {
+        super(MAIN_DIALOG_ID);
+        this.addDialog(new TextPrompt("TextPrompt"))
+            .addDialog(new TeamsInfoDialog())
+            .addDialog(new HelpDialog())
+            .addDialog(new MentionUserDialog())
+            .addDialog(new WaterfallDialog(MAIN_WATERFALL_DIALOG_ID, [
+                this.introStep.bind(this),
+                this.actStep.bind(this)
+            ]));
+        this.initialDialogId = MAIN_WATERFALL_DIALOG_ID;
+        this.onboarding = false;
+    }
+
+    public async run(context: TurnContext, accessor: StatePropertyAccessor<DialogState>) {
+        const dialogSet = new DialogSet(accessor);
+        dialogSet.add(this);
+        const dialogContext = await dialogSet.createContext(context);
+        const results = await dialogContext.continueDialog();
+        if (results.status === DialogTurnStatus.empty) {
+            await dialogContext.beginDialog(this.id);
+        }
+    }
+
+    private async introStep(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
+        console.log("Service URL:", stepContext.context.activity.serviceUrl);
+
+        
+        if ((stepContext.options as any).restartMsg) {
+            const messageText = (stepContext.options as any).restartMsg ? (stepContext.options as any).restartMsg : "What can I help you with today?";
+            const promptMessage = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+            return await stepContext.prompt("TextPrompt", { prompt: promptMessage });
+        } else {
+            this.onboarding = true;
+            return await stepContext.next();
+        }
+    }
+
+    private async actStep(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
+        console.log(1);
+        if (stepContext.result) {
+            /*
+            ** This is where you would add LUIS to your bot, see this link for more information:
+            ** https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-howto-v4-luis?view=azure-bot-service-4.0&tabs=javascript
+            */
+            const result = stepContext.result.trim().toLocaleLowerCase();
+            switch (result) {
+                case "who":
+                case "who am i?": {
+                    return await stepContext.beginDialog("teamsInfoDialog");
+                }
+                case "get help":
+                case "help": {
+                    return await stepContext.beginDialog("helpDialog");
+                }
+                case "mention me":
+                case "mention": {
+                    return await stepContext.beginDialog("mentionUserDialog");
+                }
+                default: {
+                    await stepContext.context.sendActivity("Ok, maybe next time 😉");
+                    return await stepContext.next();
+                }
+            }
+        } else if (this.onboarding) {
+            const body = {
+                question: stepContext.context.activity.text,
+                model: "gpt-4o",
+                session_id: "1"
+            };
+
+            const response = await fetch(process.env.LANGCHAIN_API_URL + "/chat", {
+                method: "POST",
+                headers: {
+                    accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body)
+            });
+            const data = await response.json();
+            console.log(data.answer);
+            await stepContext.context.sendActivity(data.answer);
+            return await stepContext.next();
+            /* switch (stepContext.context.activity.text) {
+                case "who": {
+                    return await stepContext.beginDialog("teamsInfoDialog");
+                }
+                case "help": {
+                    return await stepContext.beginDialog("helpDialog");
+                }
+                case "mention": {
+                    return await stepContext.beginDialog("mentionUserDialog");
+                }
+                default: {
+                    await stepContext.context.sendActivity("Ok, maybe next time 😉");
+                    return await stepContext.next();
+                }
+            } */ // Thêm khoảng trắng trước */
+        }
+        return await stepContext.next();
+    }
+
+    private async finalStep(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
+        return await stepContext.replaceDialog(this.initialDialogId, { restartMsg: "What else can I do for you?" });
+    }
+}
